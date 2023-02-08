7e06d66541ac43dec92350c22169e9340405ec97
Asad Hayat <asadhayat.1068@gmail.com>
2023-02-09 01:13:48 +0500

    Vue logic added

--- 01_1.7/app.js
+++ 01_1.7/app.js
@@ -1,3 +1,16 @@
+Vue.createApp({
+    data: function () {
+        return {
+            inputGoal: '',
+            goals: []
+        }
+    },
+    methods: {
+        addGoal() {
+            this.goals.push(this.inputGoal);
+        }
+    }
+});
 // const buttonEl = document.querySelector('button');
 // const inputEl = document.querySelector('input');
 // const listEl = document.querySelector('ul');
