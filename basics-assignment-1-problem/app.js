d47606506b02fd5084ff4a71a10fc03e71dffe39
Asad Hayat <asadhayat.1068@gmail.com>
2023-02-09 20:26:19 +0500

    Photo added

--- basics-assignment-1-problem/app.js
+++ basics-assignment-1-problem/app.js
@@ -3,6 +3,8 @@ const app = Vue.createApp({
     return {
       name: "Asad Hayat",
       age: 32,
+      photo:
+        "https://images.unsplash.com/photo-1570295999919-56ceb5ecca61?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MjB8fHByb2ZpbGV8ZW58MHx8MHx8&w=1000&q=80",
     };
   },
   methods: {
