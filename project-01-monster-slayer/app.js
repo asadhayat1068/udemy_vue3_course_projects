0cfa19d324c51cabd4c2fd648ab5e761e42772e7
Asad Hayat <asadhayat.1068@gmail.com>
2023-02-15 23:42:42 +0500

    - Winner Logic

--- project-01-monster-slayer/app.js
+++ project-01-monster-slayer/app.js
@@ -8,6 +8,7 @@ const app = Vue.createApp({
       playerHealth: 100,
       monsterHealth: 100,
       currentRound: 0,
+      winner: null,
     };
   },
   methods: {
@@ -48,6 +49,22 @@ const app = Vue.createApp({
     },
     isSpecialAttackEnable() {
       return this.currentRound % 3 != 0;
+    },
+  },
+  watch: {
+    playerHealth(value) {
+      if (value <= 0 && this.monsterHealth <= 0) {
+        this.winner = "draw";
+      } else if (value <= 0) {
+        this.winner = "monster";
+      }
+    },
+    monsterHealth(value) {
+      if (value <= 0 && this.playerHealth <= 0) {
+        this.winner = "draw";
+      } else if (value <= 0) {
+        this.winner = "player";
+      }
     },
   },
 });
