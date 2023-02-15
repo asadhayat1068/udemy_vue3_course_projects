5abf0c0a29f2b38445ef099a7f6122b27ea3e4af
Asad Hayat <asadhayat.1068@gmail.com>
2023-02-15 23:49:53 +0500

    Start New Game

--- project-01-monster-slayer/app.js
+++ project-01-monster-slayer/app.js
@@ -48,6 +48,12 @@ const app = Vue.createApp({
       }
       this.attackPlayer();
     },
+    newGame() {
+      this.playerHealth = 100;
+      this.monsterHealth = 100;
+      this.currentRound = 0;
+      this.winner = null;
+    },
   },
   computed: {
     monsterHealthBar() {
